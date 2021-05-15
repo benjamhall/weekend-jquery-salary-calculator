dbd39f837bdb1d777b13e012b805869071a4216e
Ben Hall <benjamhall@gmail.com>
2021-05-15 15:33:51 -0500

    add comments and update collect info function

--- client.js
+++ client.js
@@ -1,9 +1,24 @@
 console.log('js loaded');
 
-//DOM Ready
+let totalSalary = 0;
+let monthlyCosts = 0; 
+
 $(document).ready(readyNow);
+// DOM is ready for information to be addded 
 
-function readyNow() {
+function readyNow() {  //when submit button is clicked the input value is pushed to the table
     console.log('jQuery ready');
-}
+    $('#submitButton').on('click', collectInfo)
+    
+} //end readyNow
 
+function collectInfo(){  //function to add employee info to the table
+    console.log('submit button works');
+    let employeeInfo = $('.input').val();
+
+    $('#employeeData').append(`
+    <td>
+        ${employeeInfo};
+    </td>
+    `);
+} //end handleForm function 
