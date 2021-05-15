de8ad03f7e3d505ab17f5ce68a298becd5d046cd
Ben Hall <benjamhall@gmail.com>
2021-05-15 16:47:27 -0500

    add delete btn

--- client.js
+++ client.js
@@ -8,17 +8,51 @@ $(document).ready(readyNow);
 
 function readyNow() {  //when submit button is clicked the input value is pushed to the table
     console.log('jQuery ready');
+
+    //click listeners
     $('#submitButton').on('click', collectInfo)
+    
+    //dynamic click listener
+    $('#tableBody').on('click', '.deleteBtn', deleteRow)
     
 } //end readyNow
 
 function collectInfo(){  //function to add employee info to the table
     console.log('submit button works');
-    let employeeInfo = $('.input').val();
+    //let employeeInfo = $('.input').val();
 
-    $('#employeeData').append(`
-    <td>
-        ${employeeInfo};
-    </td>
+    let firstName = $('#fName').val();
+    let lastName = $('#lName').val();
+    let idNumber = $('#idNum').val();
+    let jobTitle = $('#jTitle').val();
+    let annualSalary = $('#aSalary').val();
+
+
+    $('#tableBody').append(`
+        <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${idNumber}</td>
+            <td>${jobTitle}</td>
+            <td>${annualSalary}</td>
+            <td>
+                <button class="deleteBtn">Delete</button>
+            </td>
+    </tr>
     `);
-} //end handleForm function 
+} //end collectInfo function 
+
+function deleteRow(){
+    console.log('delete button works');
+
+    //delete the whole row
+    $(this).parent().remove()
+}
+
+
+// function addRow(){
+//     rowNumber ++
+    
+// }
+
+//${ employeeInfo };
