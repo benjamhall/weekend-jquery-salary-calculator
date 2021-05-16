181fc7360cc6c84bdf92cde1406ba1b3cefb302f
Ben Hall <benjamhall@gmail.com>
2021-05-16 13:05:06 -0500

    update monthlySal function

--- client.js
+++ client.js
@@ -1,21 +1,31 @@
 console.log('js loaded');
 
-let totalSalary = 0;
-let monthlyCosts = 0; 
+
+let monthlyCosts = []; 
 
 $(document).ready(readyNow);
 // DOM is ready for information to be addded 
 
 function readyNow() {  //when submit button is clicked the input value is pushed to the table
     console.log('jQuery ready');
+    //console.log(monthlyCosts);
+    
+    //arrayLog();
 
     //click listeners
     $('#submitButton').on('click', collectInfo)
+    $('#submitButton').on('click', clearInput)
+    $('#submitButton').on('click', monthlySal);
     
     //dynamic click listener
     $('#tableBody').on('click', '.deleteBtn', deleteRow)
-    
+
+
 } //end readyNow
+
+// function arrayLog(){
+//     console.log(monthlyCosts);
+// }
 
 function collectInfo(){  //function to add employee info to the table
     console.log('submit button works');
@@ -41,13 +51,16 @@ function collectInfo(){  //function to add employee info to the table
     </tr>
     `);
 
-    //clear input
-    $('#fName').val('');
-    $('#lName').val('');
-    $('#idNum').val('');
-    $('#jTitle').val('');
-    $('#aSalary').val('');
 
+    //push to monthly costs array
+    return monthlyCosts.push(annualSalary);
+    //return console.log(monthlyCosts);
+    
+    
+    //totalSalary += +(aSalary);
+    //monthlySal();
+
+    
 
     //if any of the inputs are blank to return 
     // if (firstName === '' || lastName === '' || idNumber === '' || jobTitle === '' || annualSalary === ''){
@@ -57,6 +70,15 @@ function collectInfo(){  //function to add employee info to the table
 
 } //end collectInfo function 
 
+function clearInput(){
+    //clear input
+    $('#fName').val('');
+    $('#lName').val('');
+    $('#idNum').val('');
+    $('#jTitle').val('');
+    $('#aSalary').val('');
+}//end clearInput
+
 function deleteRow(){ //delete the whole row in the table
     console.log('delete button works');
 
@@ -64,3 +86,25 @@ function deleteRow(){ //delete the whole row in the table
     $(this).closest('.tableRow').empty()
 }
 
+// calculate monthly cost and append to DOM, if it is over 20k flag red
+function monthlySal(){
+    console.log('monthly Salary is showing up');
+
+    let totalSalary = 0;
+    let monthlySalary = 0;
+
+    for(i=0; i< monthlyCosts.length; i++){
+        totalSalary += Number(monthlyCosts[i])
+        //console.log(totalSalary);
+    }
+    
+    //calculate the monthly cost by dividing total salary by 12
+    monthlySalary = totalSalary /12;
+    $('#monthlyCost').text(`${monthlySalary}`);
+
+    //if total monthly cost exceeds 20k, flag red
+    if(monthlySalary > 20000){
+        $('#monthlyCost').addClass('highlight')
+    }//adds red background
+    //return console.log(monthlySalary);
+}       
